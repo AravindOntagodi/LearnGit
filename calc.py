170807f5704c5d9228b3eccfb8f93b61d8245f12
Aravind Ontagodi <ontagodiaravind@gmail.com>
2022-02-18 17:10:42 +0530

    added input

--- calc.py
+++ calc.py
@@ -11,5 +11,11 @@ def square(x):
     return x*x
 def squareroot(x):
     return m.sqrt(x)
-def random(r):
-    return m.random(r)
+def random(x):
+    return m.random(x)
+
+a=int(input("Enter a random number a="))
+b=int(input("Enter a random number b="))
+x=int(input("Enter x"))
+print(sub(a,b),add(a,b),mul(a,b),div(a,b))
+print(square(x),squareroot(x),random(x))
