9cc8bdf3bb45123c4b295a079a97adb6157657f1
Aravind Ontagodi <ontagodiaravind@gmail.com>
2022-02-18 20:39:40 +0530

    v1

--- calc.py
+++ calc.py
@@ -1,4 +1,8 @@
 import math as m
+import numpy as np 
+import tkinter as tk 
+import statistics
+import matplotlib as mplt
 import pandas as pd
 def add(a,b):
     return a+b
@@ -12,5 +16,11 @@ def square(x):
     return x*x
 def squareroot(x):
     return m.sqrt(x)
-def random(r):
-    return m.random(r)
+def random(x):
+    return m.random(x)
+
+a=int(input("Enter a random number a="))
+b=int(input("Enter a random number b="))
+x=int(input("Enter x"))
+print(sub(a,b),add(a,b),mul(a,b),div(a,b))
+print(square(x),squareroot(x),random(x))
